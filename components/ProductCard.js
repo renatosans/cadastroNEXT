ac54d897b3d5a3a5c371feec42f9af8ddc00cedc
renato_sans <renato@datacopy.com.br>
2022-06-28 10:56:18 -0300

    Ajustes

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -7,7 +7,7 @@ export const ProductCard = ({ product }) => {
 			<div class="p-8 border-solid border-2 rounded-lg">
 				<h2 class="text-2xl font-bold"><Link href={'#'}>{product.nome}</Link></h2>
 				<p>{product.descricao}</p>
-				<img style={{height: "250px"}} src={"data:" + product.formatoImagem + ", " + product.foto} alt={product.nome}></img>
+				<img style={{height: "200px"}} src={"data:" + product.formatoImagem + ", " + product.foto} alt={product.nome}></img>
 			</div>
 		</div>
 	);
