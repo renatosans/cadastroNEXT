148da6bc03fd6a1e8dd0e725c2f15115aa038176
renato_sans <renato@datacopy.com.br>
2022-07-05 08:20:01 -0300

    Ajustes

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,14 +1,14 @@
-import Link from 'next/link';
+import React from 'react';
 
 
 export const ProductCard = ({ product }) => {
 	return (
-		<div>
+		<>
 			<div class="p-8 border-solid border-2 rounded-lg">
-				<h2 class="text-2xl font-bold"><Link href={'#'}>{product.nome}</Link></h2>
+				<h2 class="text-2xl font-bold">{product.nome}</h2>
 				<p>{product.descricao}</p>
-				<img style={{height: "200px"}} src={"data:" + product.formatoImagem + ", " + product.foto} alt={product.nome}></img>
+				<img src={"data:" + product.formatoImagem + ", " + product.foto} alt={product.nome}></img>
 			</div>
-		</div>
-	);
-};
+		</>
+	)
+}
