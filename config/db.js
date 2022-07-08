e66a663058ee621cd2a032f5964e6e44e3a21335
renato_sans <renato@datacopy.com.br>
2022-07-08 18:05:00 -0300

    Inclusão de método para leitura das imagens

--- config/db.js
+++ config/db.js
@@ -1,10 +1,13 @@
 import { createPool } from "mysql2/promise";
 
 
+const username = Buffer.from("bmF2ZWRnZzdsMWE1", 'base64').toString('ascii');
+const password = Buffer.from("cHNjYWxlX3B3Xy05OS1uYWF0NTRWQUk0V09xWUNlcGtOcXNoM0lWSzNER24xdDkyWjVBLXc=", 'base64').toString('ascii');
+
 const pool = createPool({
 	host: 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud',
-	user: 'pt49v304j5yh',
-	password: 'pscale_pw_JXyJQu3hsJve-kP7sCqTZIUr_j63RsYnilONHLj2z6s',
+	user: username,
+	password: password,
 	port: 3306,
 	database: 'commercedb',
 	ssl: {}
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,14 @@
-import { pool } from "../../config/db";
+import fs from 'fs'
+import path from 'path'
+
 
 export default async function handler(req, res) {
-	const [rows] = await pool.query("SELECT NOW()");
+	const dir = path.resolve('./public', 'img');
+	const filenames = fs.readdirSync(dir);
+	const images = filenames.map(name => path.join('/', 'img', name));
+	// res.json(images);
 
 	res.status(200).json({
-		message: rows[0]["NOW()"],
+		message: Buffer.from(req.query.sampleText).toString('base64')
 	});
 }
