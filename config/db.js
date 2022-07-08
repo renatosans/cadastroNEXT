7c2a3c3f1c27b2545d45a9ea6aa0b4f89f2e58fc
renato_sans <renato@datacopy.com.br>
2022-07-08 14:03:03 -0300

    Ajustes

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ import { createPool } from "mysql2/promise";
 
 const pool = createPool({
 	host: 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud',
-	user: '22affjlnzuvx',
-	password: 'pscale_pw_9smLVitPTJpAY1VuFJi5AQMZP3-TQdFDoSlN_vU9mMw',
+	user: 'root',
+	password: '',
 	port: 3306,
 	database: 'commercedb',
 	ssl: {}
