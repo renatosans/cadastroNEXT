cd4156d6a4d6cc8231e55536bb2fb4ef563697b2
renato_sans <renato@datacopy.com.br>
2022-07-08 19:26:22 -0300

    Ajustes

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
 import { createPool } from "mysql2/promise";
 
 
-const username = Buffer.from("bmF2ZWRnZzdsMWE1", 'base64').toString('ascii');
-const password = Buffer.from("cHNjYWxlX3B3Xy05OS1uYWF0NTRWQUk0V09xWUNlcGtOcXNoM0lWSzNER24xdDkyWjVBLXc=", 'base64').toString('ascii');
+const username = Buffer.from('czlsZjFkbWplNjdr', 'base64').toString('ascii');
+const password = Buffer.from('cHNjYWxlX3B3X1dpN3Z6NkhXV2d5bWYyRW50MTJMV0lPT29LQ1Rsa2lqR1ZTSEU1N0lveG8=', 'base64').toString('ascii');
 
 const pool = createPool({
 	host: 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud',
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -10,5 +10,5 @@ export default async function handler(req, res) {
 
 	res.status(200).json({
 		message: Buffer.from(req.query.sampleText).toString('base64')
-	});
+	})
 }
