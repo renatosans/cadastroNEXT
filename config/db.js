581624b56673d4139db8b99001d556cc7af1c6a5
renato_sans <renato@datacopy.com.br>
2022-07-09 10:16:31 -0300

    Correção de bug

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ import { createPool } from "mysql2/promise";
 
 
 // dados para conexão com banco de dados serverless
+// caso o servidor encontre a senha no github ele revoga a senha
 const host = 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud';
 const username = Buffer.from('YXZtYmkxZ2h6b3Vs', 'base64').toString('ascii');
 const password = Buffer.from('cHNjYWxlX3B3X3VXb2NsNU5ONlRNeG5jb2VjUzhrQ0o1TEZuVndHQm1USktyOXdSYWpfb2c=', 'base64').toString('ascii');
