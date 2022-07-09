e9bc69a0fd213f2f0ee89d72b38099aac7005e70
renato_sans <renato@datacopy.com.br>
2022-07-09 10:13:38 -0300

    Correção de bug

--- config/db.js
+++ config/db.js
@@ -1,16 +1,18 @@
 import { createPool } from "mysql2/promise";
 
 
-const username = Buffer.from('czlsZjFkbWplNjdr', 'base64').toString('ascii');
-const password = Buffer.from('cHNjYWxlX3B3X1dpN3Z6NkhXV2d5bWYyRW50MTJMV0lPT29LQ1Rsa2lqR1ZTSEU1N0lveG8=', 'base64').toString('ascii');
+// dados para conexão com banco de dados serverless
+const host = 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud';
+const username = Buffer.from('YXZtYmkxZ2h6b3Vs', 'base64').toString('ascii');
+const password = Buffer.from('cHNjYWxlX3B3X3VXb2NsNU5ONlRNeG5jb2VjUzhrQ0o1TEZuVndHQm1USktyOXdSYWpfb2c=', 'base64').toString('ascii');
 
 const pool = createPool({
-	host: 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud',
-	user: username,
-	password: password,
+	host: 'localhost',
+	user: 'root',
+	password: 'p@ssw0rd',
 	port: 3306,
 	database: 'commercedb',
-	ssl: {}
+	// ssl: {}
 })
 
 export { pool }
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,17 @@
+import axios from 'axios';
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
-import toast, { Toaster } from "react-hot-toast";
 import { Layout } from '../components/Layout';
 import { ProductCard } from '../components/ProductCard';
 import { ProductList } from '../components/ProductList';
 
 
-const HomePage = () => {
-	const [produtos, setProdutos] = useState([]);
-
-	const getProdutos = async () => {
-		const response = await fetch('api/products')
-		.then((response) => response.json());
-
-		setProdutos(response);
-	}
-
-	useEffect(() => {
-		getProdutos();
-	}, []);
-
+const HomePage = ({ products }) => {
 	return (
 		<Layout>
-			<Toaster />
-			<ProductList products={produtos}></ProductList>
+			<ProductList products={products}></ProductList>
 
 			<div className="grid gap-4 grid-cols-1 md:grid-cols-3"> {
-				produtos && produtos.map( (product) => 
+				products.map( (product) => 
 					<Link href={`products/${product.id}`} key={product.id}>
 						<ProductCard produto={product} />
 					</Link>
@@ -34,6 +19,17 @@ const HomePage = () => {
 			} </div>
 		</Layout>
 	)
+}
+
+export const getServerSideProps = async (context) => {
+	const route = 'http://' + context.req.headers.host + '/api/products';
+	const { data: products } = await axios.get(route);
+
+	return {
+		props: {
+			products,
+		},
+	}
 }
 
 export default HomePage
