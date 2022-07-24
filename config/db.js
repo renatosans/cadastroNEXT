afaae62855d4060e489e81b11d3a3466cad4b1d3
renato_sans <renato@datacopy.com.br>
2022-07-24 18:06:29 -0300

    Ajustes

--- config/db.js
+++ config/db.js
@@ -1,18 +1,12 @@
 import { createPool } from "mysql2/promise";
 
-
-// dados para conexão com banco de dados serverless
-const host = 'k3cizowdet74.aws-sa-east-1-1.psdb.cloud';
-const username = Buffer.from('ZGttbXkzdjZ6dzZq', 'base64').toString('ascii');
-const password = Buffer.from('cHNjYWxlX3B3XzMyYlFkYVI2NWdWalg2QUJzampzUUZkVk92anhnN2JZajJONXc5bnNMSjQ=', 'base64').toString('ascii');
-
 const pool = createPool({
-	host: host,
-	user: username,
-	password: password,
+	host: 'localhost',
+	user: 'root',
+	password: 'p@ssw0rd',
 	port: 3306,
 	database: 'commercedb',
-	ssl: {}
+	// ssl: {}
 })
 
 export { pool }
