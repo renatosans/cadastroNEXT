4b01a4ac7c4d075e8db0c88d19c6d0032cdf55c5
renato_sans <renato@datacopy.com.br>
2022-07-31 12:27:19 -0300

    Ajustes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -3,6 +3,11 @@ import path from 'path'
 
 
 export default async function handler(req, res) {
+	if ( !req.query.sampleText ) {
+        res.send('Erro: parametro precisa ser informado!');
+        return;
+    }
+
 	const dir = path.resolve('./public', 'img');
 	const filenames = fs.readdirSync(dir);
 	const images = filenames.map(name => path.join('/', 'img', name));
