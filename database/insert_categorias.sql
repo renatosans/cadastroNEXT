d436c38666cef1cdbdd530d259784370607efa67
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-09-09 11:38:56 -0300

    Ajustes

---
+++ database/insert_categorias.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO categoria (id, nome) VALUES (1, 'Móveis');
+INSERT INTO categoria (id, nome) VALUES (2, 'Eletrônicos');
+INSERT INTO categoria (id, nome) VALUES (3, 'Relógios');
+INSERT INTO categoria (id, nome) VALUES (4, 'Celulares');
+INSERT INTO categoria (id, nome) VALUES (5, 'Artigos Esportivos');
