917f5311195001df0b5c436f5655f77e66f4d814
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-30 11:40:29 -0300

    Ajustes

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ import { createPool } from "mysql2/promise";
 
 // dados da conexão com BD serverless
 const pool = createPool({
-	host: 'us-east.connect.psdb.cloud',
-	user: Buffer.from('OHMzdHlqNXk2dzA2bDQyY202cDM=', 'base64').toString('ascii'),
-	password: Buffer.from('cHNjYWxlX3B3X1B6cVpoUzk5NGxJcmFaeGdyQzdwSmVrS1VuT3RocGNFZzFJV1lZblpDRTQ=', 'base64').toString('ascii'),
+	host: 'localhost',
+	user: 'root',
+	password: 'p@ssw0rd',
 	port: 3306,
 	database: 'commercedb',
-	ssl: {}
+	// ssl: {}
 })
 
 export { pool }
